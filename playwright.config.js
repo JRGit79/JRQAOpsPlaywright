a04418e40eb4f89f99c25ad71828fdccf1aa560e
Jitendra Rawat <mail4jrawat@gmail.com>
2026-09-16 16:39:18 +0530

    update

--- playwright.config.js
+++ playwright.config.js
@@ -1,39 +1,26 @@
 // @ts-check
 import { defineConfig, devices } from '@playwright/test';
-import { trace } from 'console';
 
-const config = ({
+export default defineConfig({
   testDir: './tests',
   retries: 1,
   
-  //retries :1,
+  /* Maximum time one test can run for */
+  timeout: 31 * 1000,
   
-  // global time out for scripts default timeout: 30*1000
-  
-  /*Maximum time one test can run for. */
-  timeout: 31*1000,
-  
-  // Time out for exclusively assertions default timeout: 5000
+  /* Timeout for assertions */
   expect: {
     timeout: 6000
   },
   
   reporter: 'html',
-  
 
-  //shared settings for all the project as below
+  /* Shared settings for all the projects */
   use: {
-       browserName: 'chromium',
-       headless: true,
-       screenshot:  'on',
-       trace: 'retain-on-failure', 
-       //trace: 'retain-on-failure ' // on // off
-
-      
-       },
-
-     
+    browserName: 'chromium',
+    channel: 'chrome',              // <-- FORCES PLAYWRIGHT TO USE YOUR LOCAL GOOGLE CHROME
+    headless: true,
+    screenshot: 'on',
+    trace: 'retain-on-failure', 
+  },
 });
-module.exports = config;
-
-
